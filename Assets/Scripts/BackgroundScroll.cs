430ef8d66c99f705f12c1e89a816c2206fc7512c
Zachary Robertson <ztrsec@hotmail.com>
2017-09-15 23:05:09 +1000

    Uploading conditions for BackgroundScroll

--- Assets/Scripts/BackgroundScroll.cs
+++ Assets/Scripts/BackgroundScroll.cs
@@ -6,6 +6,10 @@ public class BackgroundScroll : MonoBehaviour
 {
     public float scrollSpeed = 0.25f;
     private Renderer rend;
+
+    public bool isForest, isLake, isCastle;
+    public Material[] materials;
+
     // Use this for initialization
     void Start()
     {
@@ -15,6 +19,21 @@ public class BackgroundScroll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(isForest && !isLake && !isCastle)
+        {
+            rend.material = materials[0];
+        }
+
+        else if(isLake && !isForest && !isCastle)
+        {
+            rend.material = materials[1];
+        }
+
+        else if (isCastle && !isForest && !isLake)
+        {
+            rend.material = materials[2];
+        }
+
         float offset = Time.time * scrollSpeed;
         rend.material.SetTextureOffset("_MainTex", new Vector2(0, -offset));
     }
