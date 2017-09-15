8dfcc544104516afdc97d83200d58eca35b91a0f
ZacharyRobertson <ztrsec@hotmail.com>
2017-09-15 21:44:43 +1000

    Updating basic background scroll

--- Assets/Scripts/BackgroundScroll.cs
+++ Assets/Scripts/BackgroundScroll.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BackgroundScroll : MonoBehaviour {
+public class BackgroundScroll : MonoBehaviour
+{
+    public float scrollSpeed = 0.25f;
+    private Renderer rend;
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(0, -offset));
+    }
 }
